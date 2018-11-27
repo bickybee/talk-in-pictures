7ad3cb71f65d17cceb2dc444e5438cd0d460d080
Elaine Huynh <elaine.huynh@mail.utoronto.ca>
2018-11-27 17:47:11 -0500

    Changed to vertical scrolling and added modal window for icon selection (but don't have actual selection yet)

--- python-sandbox/static/main.js
+++ python-sandbox/static/main.js
@@ -1,6 +1,17 @@
 "use strict";
 
 var recognition;
+var modal;
+var span;
+
+var iconModal;
+var iconSpan;
+
+var iconSlider;
+var fontSlider;
+var baseIconSize = 200; // in px
+
+var currIdx = 0;
 
 /* Format and send JSON to the server */
 function sendParse(sentence, phraseNum){
@@ -24,16 +35,42 @@ function handleParse(raw) {
     for (var i in phrase) {
         // Ignore the grim reaper:
         if (phrase[i]["word"].trim() != "") {
+            var id = "" + currIdx + "-" + phrase[i]["word"];
+
             // Create new container:
-            var new_item = '<div class="tkn-container">' +
+            var new_item = '<div class="tkn-container" id="' + id + '">' +
                 '<div class="tkn-img"><img src="' + phrase[i]["img"] + '"></img></div>' +
                 '<div class="tkn-caption">' + phrase[i]["word"] + '</div>' +
                 '</div>';
 
+            currIdx++;  // generate unique id for this token (#-word, where # = currIdx)
+
             $('#output').append(new_item);
+            $(('#' + id)).click(displayAlternatives);
+            // scrollRight();
         }
     }
 }
+
+function displayAlternatives() {
+    // Display the modal window:
+    $("#iconModal").css("display", "block");
+
+    // Popula
+    $("#alternativeIcons").html(this.id);
+}
+
+function scrollRight() {
+    var targetElement = $('.container');
+    var speed = 1000;
+
+    var scrollWidth = $(targetElement).get(0).scrollWidth;
+    var clientWidth = $(targetElement).get(0).clientWidth;
+    $(targetElement).animate({ scrollLeft: scrollWidth - clientWidth },
+    {
+        duration: speed
+    });
+};
 
 /* Handles WebkitSpeechRecognition event  */
 function handleRecognitionResult(event) {
@@ -76,7 +113,6 @@ function startDictation() {
         recognition.lang = "en-US";
         recognition.start();
 
-        // This thing doesn't stop at the moment:
         recognition.onresult = handleRecognitionResult;
         recognition.onerror = handleRecognitionError;
     } else {
@@ -96,5 +132,81 @@ $(document).ready(function() {
     $('#input').bind('input propertychange', function() {
         sendParse($(this).val());
     })
+
+    // Set up modal window variables:
+    modal = document.getElementById('myModal');
+    span = document.getElementById("modalClose");
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Set up modal window variables:
+    iconModal = document.getElementById('iconModal');
+    iconSpan = document.getElementById("iconModalClose");
+
+    // When the user clicks on <span> (x), close the modal
+    iconSpan.onclick = function() {
+        iconModal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        } else if (event.target == iconModal) {
+            iconModal.style.display = "none";
+        }
+    }
+
+    // Set up sliders:
+    iconSlider = document.getElementById("iconRange");
+    fontSlider = document.getElementById("fontRange");
+
+    iconSlider.oninput = function() {
+        var newVal = baseIconSize * this.value / 100;
+        var images = $(".tkn-img");
+
+        for (var i in images) {
+            ($(images[i])).css("width", newVal);
+            ($(images[i])).css("height", newVal);
+        }
+    }
+
+    fontSlider.oninput = function() {
+        var captions = $(".tkn-caption");
+
+        for (var i in captions) {
+            ($(captions[i])).css("fontSize", "" + this.value + "px");
+        }
+    }
 })
 
+$(document).on("keypress", function (e) {
+    switch(e.keyCode) {
+        // enter
+        case 13:
+            scrollRight();
+            break;
+
+        // space
+        case 32:
+            e.preventDefault();
+
+            if (iconModal.style.display == "block") { 
+                iconModal.style.display = "none";
+            }
+
+            if (modal.style.display == "block") {
+                modal.style.display = "none";
+            } else {
+                modal.style.display = "block";
+            }
+
+            break;
+
+        default:
+            return;
+    }
+})
