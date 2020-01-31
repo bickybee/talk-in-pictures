e63363b890595441b5d1daa94b5c328cd2f67297
Vicky <bilbilv@mcmaster.ca>
2020-01-31 18:32:47 -0500

    Mic permissions request

--- static/main.js
+++ static/main.js
@@ -273,6 +273,14 @@ $(document).ready(function() {
             ($(captions[i])).css("fontSize", "" + this.value + "px");
         }
     }
+                  
+  navigator.mediaDevices.getUserMedia({ audio: true })
+  .then(function(stream) {
+    console.log('You let me use your mic!')
+  })
+  .catch(function(err) {
+    console.log('No mic for you!')
+  });
 })
 
 $(document).on("keypress", function (e) {
